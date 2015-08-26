2788d98cf9c48ad208b0ea51cd2355f4e645d0e1
omiqbaltawakkal <tawakkal.iqbal96@gmail.com>
2015-08-26 20:40:52 +0700

    Update exercise4.cpp

---
+++ exercise4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int i;
+int a=0;
+while (n=! 9999){
+cout<< "please input a value : ";
+cin >> i;
+if i >> a
+  a==i;
+}
+cout<<"minimum :" "<<a"
+
+}
